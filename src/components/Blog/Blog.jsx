93461f24818261755b878e476c6f088f437f4ae5
Farjana <farjanajhn12@gmail.com>
2023-04-10 12:30:57 +0200

    given questions answer in blog component

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <div>
-      <h1>Here is blog</h1>
+    <div className='mt-12'>
+       <h2 className='text-4xl font-semibold text-center'>Blog</h2>
+      <p className='text-center mt-4'>You would find most important question answer about react router</p>
+      <div className='mx-12 text-center border-2 border-purple-400 rounded mt-12 pb-4'>
+        <h1 className='text-2xl font-semibold text-center mt-4 '>When we have to use context API?</h1>
+        <p className='text-center mt-4'>when we need to pass data from a parent component down to a child component, but that data is not used by any other components in between. If we have data that multiple components in our application need to access we can also use Context API.</p>
+
+        <h1 className='text-2xl font-semibold text-center mt-4'>What is coustom hook?</h1>
+        <p className='text-center mt-4'>A custom hook in React is a JavaScript function that allows you to reuse stateful logic across multiple components. It's a way to extract stateful logic from a component so that it can be reused in other components.Custom hooks follow a specific naming convention: their names should start with "use". This naming convention is important because it lets React know that this function is a hook and that it follows the rules of hooks.</p>
+        <h1 className='text-2xl font-bold text-center mt-4'>What is useRef?</h1>
+
+        <p className='text-center mt-4'>useRef is a built-in hook in React that returns a mutable ref object. Refs are a way to access and modify the properties of a DOM element or a child component directly. The ref object can be passed as a prop to a child component or used directly in a function or method.</p>
+
+
+      </div>
     </div>
   );
 };
--- src/components/ErrorHandle/ErrorHandle.jsx
+++ src/components/ErrorHandle/ErrorHandle.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const ErrorHandle = () => {
   return (
-    <div>
-      <h1 className='text-purple-900 text-center font-bold text-9xl my-56'> 404</h1>
+    <div className='my-56'>
+      <h1 className='text-purple-900 text-center font-bold text-9xl'>404</h1>
+
     </div>
   );
 };
